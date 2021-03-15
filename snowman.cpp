053b9d9b4628d066ca6a118a320f29fc053d3793
daniel <dansela123@gmail.com>
2021-03-15 21:25:28 +0200

    3 commit

--- snowman.cpp
+++ snowman.cpp
@@ -4,41 +4,52 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <array>
 using namespace std;
 
 namespace ariel{
 
     std::string snowman(int x){
         vector<int> number;
-        const seven=7;
-        const six=6;
-        const five=5;
-        const eight=8;
-        const ten=10;
-        const tesh=9;
-        const one=11111111;
-        const four=44444444;
         int num1=x;
         int num2=0;
+        // magic number 
+        const int basee=7;
+        const int yy=5;
+        const int torsos=6;
+        const int cou=8;
+        const int numbb=10;
+        const int check=44444444;
+        const int check2=11111111;
+        const int cou2=9;
+
         int count=1;
-        if(x<11111111 || x > 44444444 )
+
+        // check if number good 
+        if(x<check2 || x > check )
         {
-            throw std::out_of_range("canot");
+            throw std::out_of_range("Incorrect typing");
         }
-        while (num1>0 && count<9)
+
+        // input number to vextor and check if its good
+        while (num1>0 && count<cou2)
         {
-            num2=num1%10;
-            if(num2>4 || num2<1 || count>8){
-                throw std::out_of_range("canot");
+            num2=num1%numbb;
+            if(num2>4 || num2<1 || count>cou){
+                throw std::out_of_range("Incorrect typing");
             }
-                num1=num1/10;
+                num1=num1/numbb;
                 count++;
                 number.push_back(num2);
             
         }
+
+        // build the snowman
         count=1;
+        int x_=0;
+        int y_=0;
         // num1=0;
-        string arr[8];
+        std::array<string,cou> arr;
         while (!number.empty())
         {
             num1=number.back();
@@ -61,40 +72,79 @@ namespace ariel{
                     arr[3]=right(num1);
                     count++;
                     break;
-                case 5:
-                    arr[4]=get_x(num1);
+                case yy:
+                    x_=num1;
+                    arr[4]=get_x(num1); 
                     count++;
                     break;
-                case 6:
-                    arr[5]=get_y(num1);
+                case torsos:
+                    y_=num1;
+                    arr[yy]=get_y(num1);
                     count++;
                     break;
-                case 7:
-                    arr[6]=torso(num1);
+                case basee:
+                    arr[torsos]=torso(num1);
                     count++;
                     break;
-                case 8:
-                    arr[7]=base(num1);
+                case cou:
+                    arr[basee]=base(num1);
                     count++;
                 default:
                     break;
             }
         }
-        return " "+arr[0]+"\n"+(arr[4]+"("+arr[2]+arr[1]+arr[3]+")"+arr[5])+"\n"+(arr[4]+arr[6]+arr[5])+"\n"+" "+arr[7];
+        // check up and down arm and return the string
+        if(x_ == 2 && y_==2){
+            return " "+arr[0]+"\n"+(arr[4]+"("+arr[2]+arr[1]+arr[3]+")"+arr[yy])+"\n"+(" "+arr[torsos]+" ")+"\n"+" "+arr[basee];
+        }
+        if(x_==2 && y_==3){
+            return " "+arr[0]+"\n"+(arr[4]+"("+arr[2]+arr[1]+arr[3]+")"+" ")+"\n"+(" "+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
+        }
+         if(x_ == 2 && y_==1){
+            return " "+arr[0]+"\n"+(arr[4]+"("+arr[2]+arr[1]+arr[3]+") ")+"\n"+(" "+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
+        }
+        if(x_==2 && y_==4){
+            return " "+arr[0]+"\n"+(arr[4]+"("+arr[2]+arr[1]+arr[3]+")"+" ")+"\n"+(" "+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
+        }
+        if(x_ == 3 && y_==2){
+            return " "+arr[0]+"\n"+(" ("+arr[2]+arr[1]+arr[3]+")"+arr[yy])+"\n"+(arr[4]+arr[torsos]+" ")+"\n"+" "+arr[basee];
+        }
+        if(x_==3 && y_==3){
+            return " "+arr[0]+"\n"+(" ("+arr[2]+arr[1]+arr[3]+")"+" ")+"\n"+(arr[4]+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
+        }
+         if(x_ == 3 && y_==1){
+            return " "+arr[0]+"\n"+(" ("+arr[2]+arr[1]+arr[3]+") ")+"\n"+(arr[4]+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
+        }
+        if(x_==3 && y_==4){
+            return " "+arr[0]+"\n"+(" ("+arr[2]+arr[1]+arr[3]+")"+" ")+"\n"+(arr[4]+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
+        }
+                if(x_ == 2 && y_==2){
+            return " "+arr[0]+"\n"+(arr[4]+"("+arr[2]+arr[1]+arr[3]+")"+arr[yy])+"\n"+(" "+arr[torsos]+" ")+"\n"+" "+arr[basee];
+        }
+        if((x_==1 || x_==4)  && y_==2){
+            return " "+arr[0]+"\n"+(" ("+arr[2]+arr[1]+arr[3]+")"+arr[yy])+"\n"+(arr[4]+arr[torsos]+" ")+"\n"+" "+arr[basee];
+        }
+         if((x_==1 || x_==4) && y_==3){
+            return " "+arr[0]+"\n"+(" ("+arr[2]+arr[1]+arr[3]+") ")+"\n"+(arr[4]+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
+        }
+
+
+        
+            return " "+arr[0]+"\n"+(" ("+arr[2]+arr[1]+arr[3]+")"+" ")+"\n"+(arr[4]+arr[torsos]+arr[yy])+"\n"+" "+arr[basee];
     }
 
-
-    std::string hat(int num){
+    // func that return hat or arm or ......
+    std::string hat(int num){ 
         switch (num)
         {
             case 1:
                 return "_===_";
             case 2:
-                return "___ \n....";
+                return " ___ \n .....";
             case 3:
                 return "  _  \n  /_\\";
             case 4:
-                return " ___ \n(_*_)";
+                return " ___ \n (_*_)";
 
             default:
                 return "";
@@ -156,7 +206,7 @@ namespace ariel{
             case 1:
                 return "<";
             case 2:
-                return "\\"; //92
+                return "\\"; 
             case 3:
                 return "/";
             case 4:
@@ -204,7 +254,7 @@ namespace ariel{
             case 1:
                 return "( : )";
             case 2:
-                return "(" ")";
+                return "(\" \")";
             case 3:
                 return "(___)";
             case 4:
